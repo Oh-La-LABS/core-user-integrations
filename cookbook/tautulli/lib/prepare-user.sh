9cc2d5f641af8007e93580238804384113504d0a
markus-li <markus@oh-lalabs.com>
2022-10-19 10:09:17 +0800

    Update: Updated Tautulli

--- cookbook/tautulli/lib/prepare-user.sh
+++ cookbook/tautulli/lib/prepare-user.sh
@@ -4,21 +4,16 @@
 ## Runs as a regular user, NOT root!
 ############################################################
 
-############################################################
-### If not used by cui-tautulli, DELETE THIS FILE!
-############################################################
-
-## Example:
 if [ "$COMMAND" == "prepare" ]; then
   info "Finalizing preparations for the ${NAME} container as the podman user..."
 
   umask 0037
   if [ ! -s $DATA_FOLDER/config/config.ini ]; then
     cp -f /etc/default/core/{{manifest.name}}/config.ini $DATA_FOLDER/config/config.ini
+    podman unshare chown 1000:0 $DATA_FOLDER/config/config.ini
   fi
   
   umask 0022
 
-  # info "Done!"
-  # exit 0
+  info "Done!"
 fi
