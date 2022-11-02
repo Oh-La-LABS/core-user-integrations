50a20bf13b8e37d198ec88519cb9650e80ad5fe3
markus-li <markus@oh-lalabs.com>
2022-11-02 19:34:41 +0800

    Added better support for non-podman based Integrations

---
+++ templates/integration_lib/additional-arguments.sh
@@ -0,0 +1,20 @@
+############################################################
+## Custom script argument parsing belong in this file.                 
+############################################################
+
+############################################################
+### If not used by {{installer.command_name}}, DELETE THIS FILE!
+############################################################
+
+## Example:
+# FORCE=false
+
+# additional_arguments() {
+#   # return the number needed to shift
+#   case "${1}" in
+#     --force)                    FORCE=true; return 1;;
+#     --has-backup)               COMMAND="has-backup"; return 1;;
+#     --data-reset)               COMMAND="data-reset"; return 1;;
+#     *)                          return 0;;
+#   esac
+# }
---
+++ templates/integration_lib/script-usage.sh
@@ -0,0 +1,11 @@
+############################################################
+## Custom script argument definitions belong in this file.                 
+############################################################
+
+############################################################
+### If not used by {{installer.command_name}}, DELETE THIS FILE!
+############################################################
+
+## Examples:
+#SCRIPT_ARG+=("configure")
+#SCRIPT_ARG_DESC["configure"]="Configure Grafana with the defaults (create the oh-la user etc)"
